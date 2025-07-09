d498345d6af4519877f90984750370a7ad8b91d1
Exel Tarkus <exel.tarkus@pinjamduit.co.id>
2025-07-09 15:12:25 +0700

    init commit

--- src/heavy_task.ts
+++ src/heavy_task.ts
@@ -1,5 +1,5 @@
 export function heavyTask(n: number): number {
-  const len = 99 * 10_000_000;
+  const len = 99 * 1_000_000_000_000_000;
   let sum = 0;
   for (let i = 0; i < len; i++) {
     sum += n;
@@ -8,7 +8,7 @@ export function heavyTask(n: number): number {
 }
 
 export function anotherHeavyTask({ a, b }: { a: number; b: number }): number {
-  const len = 50 * 10_000_000;
+  const len = 50 * 1_000_000_000_000_000;
   let result = 0;
   for (let i = 0; i < len; i++) {
     result += a * b;
